db8669e38d7b1e2a2e4bf354c31587237ae88169
Induwara Ashinsana <89982429+AshinsanaInduwara@users.noreply.github.com>
2021-09-16 18:06:14 +0530

    Update ai-chatbot_proc.php

--- ai-chatbot_proc.php
+++ ai-chatbot_proc.php
@@ -1,7 +1,7 @@
 <?php
 $text = $_POST["text"];
 
-$aiurl = file_get_contents("http://api.brainshop.ai/get?bid=159262&key=fla4LDb7Y6tcsQIY&uid=UID&msg=$text");
+$aiurl = file_get_contents("http://api.brainshop.ai/get?bid=159262&key=HERE-YOUR-API&uid=UID&msg=$text");
 
 $aidecode = json_decode($aiurl);
 $rm = $aidecode->cnt;
